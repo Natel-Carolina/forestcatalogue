8a0e1bdcdae9a366aeec3a11c337a65ee3b64d63
Michael Hobbs <michael@hobbs.im>
2023-02-26 19:34:26 +0100

    feat: add base path plugin to allow deploying to a real domain

--- _config.ts
+++ _config.ts
@@ -1,11 +1,14 @@
 import lume from 'lume/mod.ts';
 import jsx from 'lume/plugins/jsx_preact.ts';
 import postcss from 'lume/plugins/postcss.ts';
+import basePath from 'lume/plugins/base_path.ts';
+import sitemap from 'lume/plugins/sitemap.ts';
+import terser from 'lume/plugins/terser.ts';
 
 const site = lume({
   src: './src',
 });
 
-site.use(jsx()).use(postcss());
+site.use(jsx()).use(postcss()).use(basePath()).use(terser()).use(sitemap());
 
 export default site;
