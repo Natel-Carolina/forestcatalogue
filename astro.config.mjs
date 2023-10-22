7a54ca427adb983bc839f90c3ccaf38ff4f3e565
Michael Hobbs <michael@hobbs.im>
2023-10-22 21:05:38 +0200

    fix: let astro dployment script set the site and base
    it seems the deployment script should set these automatically:
    - site "${{ steps.pages.outputs.origin }}"
    - base "${{ steps.pages.outputs.base_path }}"

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,7 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
-const site = process.env.GITHUB_REPOSITORY_OWNER
-  ? `https://${process.env.GITHUB_REPOSITORY_OWNER}.github.io`
-  : '';
-const base = process.env.GITHUB_REPOSITORY
-  ? process.env.GITHUB_REPOSITORY.split('/')[1]
-  : '';
 // https://astro.build/config
 export default defineConfig({
-  site,
-  base,
   integrations: [tailwind()],
 });
