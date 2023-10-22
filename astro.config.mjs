194321cfda045cf9a6b88b5510bc3ff4057058f6
Michael Hobbs <michael@hobbs.im>
2023-10-22 20:48:16 +0200

    ci: use github variables for github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
+const site = process.env.GITHUB_REPOSITORY_OWNER
+  ? `https://${process.env.GITHUB_REPOSITORY_OWNER}.github.io`
+  : '';
+const base = process.env.GITHUB_REPOSITORY
+  ? process.env.GITHUB_REPOSITORY.split('/')[1]
+  : '';
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com' // TODO: update with deployment URL
-  ,
-  integrations: [tailwind()]
+  site,
+  base,
+  integrations: [tailwind()],
 });
