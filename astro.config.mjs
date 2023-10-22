46b126dca950b530c98dc556d0c62c5bd9ece21c
Michael Hobbs <michael@hobbs.im>
2023-10-22 21:22:47 +0200

    ci: adjust links to be compatible with base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,14 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+const localBuildConfig = process.env.CI
+  ? {}
+  : {
+      site: 'http://localhost:4321',
+      base: '/forest-catalog',
+    };
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  ...localBuildConfig,
 });
