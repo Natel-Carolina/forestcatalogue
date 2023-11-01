24e32b65357569f8c4d90a28d530cb5abb3fda77
Michael Hobbs <michael@hobbs.im>
2023-11-01 20:21:19 +0100

    fix: rss feed urls

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -7,15 +7,16 @@ export async function GET(context) {
   const allResources = await getCollection("other-resources");
   const allTools = await getCollection("tools");
   const posts = [...allDatasets, ...allModels, ...allResources, ...allTools];
+
   return rss({
     title: "Forest Catalog",
     description: "A collection of resources for working with forest data",
-    site: context.site,
+    site: context.site.href,
     items: posts.map((post) => ({
       title: post.data.title,
       pubDate: post.data.pubDate ?? new Date(),
       description: post.data.description,
-      link: `/posts/${post.slug}/`,
+      link: `${context.site.pathname}/${post.collection}/${post.slug}/`,
     })),
     customData: `<language>en-us</language>`,
   });
