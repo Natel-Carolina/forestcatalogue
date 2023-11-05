33ffcfa0610e857b1160133f1bc0b7325c9ef6d7
Michael Hobbs <michael@hobbs.im>
2023-11-05 11:49:05 +0100

    styles: adapt inline-code and bullet styles
    - mimick github's inline-code styles, adjusted for contrast in our color scheme
    - adjust bullets color for our color scheme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -18,6 +18,31 @@ export default {
           950: "#1d2310",
         },
       },
+      typography(theme) {
+        return {
+          DEFAULT: {
+            css: {
+              "code::before": {
+                content: "none", // don’t generate the pseudo-element
+              },
+              "code::after": {
+                content: "none",
+              },
+              code: {
+                backgroundColor: theme("colors.stone.100"),
+                fontWeight: "normal",
+                fontSize: "85%",
+                borderRadius: theme("borderRadius.DEFAULT"),
+                paddingLeft: theme("spacing.1"),
+                paddingRight: theme("spacing.1"),
+                paddingTop: theme("spacing.1"),
+                paddingBottom: theme("spacing.1"),
+              },
+              "--tw-prose-bullets": theme("colors.gray.500"),
+            },
+          },
+        };
+      },
     },
   },
   plugins: [require("@tailwindcss/typography")],
